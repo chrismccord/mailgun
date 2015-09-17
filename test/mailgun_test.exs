c73a0ce455c3763dd7a0fb71c7fa131ab08a1464
Adam Rutkowski <hq@mtod.org>
2015-09-17 16:34:04 +0200

    Adjust tests to the macro-less API
    Because configs are set per test, it is required
    to stop/unload the application after each test,
    so that parallel runs don't clash.

--- test/mailgun_test.exs
+++ test/mailgun_test.exs
@@ -5,8 +5,10 @@ defmodule MailgunTest do
   @success_json "{\n  \"message\": \"Queued. Thank you.\",\n  \"id\": \"<someuser@somedomain.mailgun.org>\"\n}"
   @error_json "{\n  \"message\": \"'to' parameter is not a valid address. please check documentation\"\n}"
 
-  setup_all do
+  setup do
     Mailgun.start
+
+    on_exit &Mailgun.stop/0
   end
 
   test "url returns the full url joined with the path and domain config" do
@@ -14,24 +16,15 @@ defmodule MailgunTest do
       "https://api.mailgun.net/v3/mydomain.com/messages"
   end
 
-  test "mailers can use Client for configuration automation" do
-    defmodule Mailer do
-      use Mailgun.Client, domain: "https://api.mailgun.net/v3/mydomain.test", key: "my-key"
-    end
-
-    assert Mailer.__info__(:functions) |> Enum.member?({:send_email, 1})
-
-  end
-
   test "send_email returns {:ok, response} if sent successfully" do
     config = [domain: "https://api.mailgun.net/v3/mydomain.test", key: "my-key"]
+    set_conf config
     use_cassette :stub, [url: "https://api.mailgun.net/v3/mydomain.test/messages",
                          method: "post",
                          status_code: ["HTTP/1.1", 200, "OK"],
                          body: @success_json] do
 
-      {:ok, body} = Mailgun.Client.send_email config,
-                                              to: "foo@bar.test",
+      {:ok, body} = Mailgun.Client.send_email to: "foo@bar.test",
                                               from: "foo@bar.test",
                                               subject: "hello!",
                                               text: "How goes it?"
@@ -41,14 +34,13 @@ defmodule MailgunTest do
   end
 
   test "send_email returns {:error, reason} if send failed" do
-    config = [domain: "https://api.mailgun.net/v3/mydomain.test", key: "my-key"]
+    set_conf [domain: "https://api.mailgun.net/v3/mydomain.test", key: "my-key"]
     use_cassette :stub, [url: "https://api.mailgun.net/v3/mydomain.test/messages",
                          method: "post",
                          status_code: ["HTTP/1.1", 400, "BAD REQUEST"],
                          body: @error_json] do
 
-      {:error, status, body} = Mailgun.Client.send_email config,
-                                                         to: "foo@bar.test",
+      {:error, status, body} = Mailgun.Client.send_email to: "foo@bar.test",
                                                          from: "foo@bar.test",
                                                          subject: "hello!",
                                                          text: "How goes it?"
@@ -60,15 +52,18 @@ defmodule MailgunTest do
 
   test "sending in test mode writes the mail fields to a file" do
     file_path = "/tmp/mailgun.json"
-    config = [domain: "https://api.mailgun.net/v3/mydomain.test", key: "my-key", mode: :test, test_file_path: file_path]
-    {:ok, _} = Mailgun.Client.send_email config,
-      to: "foo@bar.test",
-      from: "foo@bar.test",
-      subject: "hello!",
-      text: "How goes it?"
+    set_conf [domain: "https://api.mailgun.net/v3/mydomain.test", key: "my-key", mode: :test, test_file_path: file_path]
+    {:ok, _} = Mailgun.Client.send_email to: "foo@bar.test",
+                                         from: "foo@bar.test",
+                                         subject: "hello!",
+                                         text: "How goes it?"
 
     file_contents = File.read!(file_path)
     assert file_contents == "{\"to\":\"foo@bar.test\",\"text\":\"How goes it?\",\"subject\":\"hello!\",\"from\":\"foo@bar.test\"}"
+  end
+
+  defp set_conf(config) do
+    config |> Enum.each(fn {k, v} -> Application.put_env(:mailgun, k, v) end)
   end
 
 end
